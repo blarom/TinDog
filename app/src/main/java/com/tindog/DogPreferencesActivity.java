beca1b0bc143eeeee0198c4698c1a0b9ad70097f
Bar Lr <bar.larom@gmail.com>
2018-06-25 23:31:02 +0300

    Initial commit

---
+++ app/src/main/java/com/tindog/DogPreferencesActivity.java
@@ -0,0 +1,13 @@
+package com.tindog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DogPreferencesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dog_preferences);
+    }
+}
